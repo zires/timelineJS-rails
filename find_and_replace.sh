202bc7bb07c99bfc86d4b33d853225255faa552c
alex <alex@aizheke.com>
2013-09-29 00:15:36 +0800

    add find and replace shell. finish find and replace

---
+++ find_and_replace.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+CWD=$(pwd);
+timelineCss=$CWD'/vendor/assets/stylesheets/timelineJS/timeline.css.erb';
+darkCss=$CWD'/vendor/assets/stylesheets/timelineJS/themes/dark.css.erb';
+
+s1='url\([';
+s2="'\"]?(?!data:)([^\)";
+s3="'\"]+)['\"]?\)";
+r=$s1$s2$s3;
+
+exp='s/'$r'/<%= image_path("timelineJS\/$1") %>/g';
+
+perl -pi -e "$exp" $timelineCss;
+perl -pi -e "$exp" $darkCss;
+
